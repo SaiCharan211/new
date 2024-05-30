d4311fb5f2e9b7c09b15f475a86939bda523b5b2
Sai Charan Poosala <saicharan9071@gmail.com>
2024-05-30 12:17:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HashRouter as Router, Route,Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarItem from './components/NavbarItem/NavbarItem';
+function App() {
+  return (
+    <Router >
+      <NavbarItem/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/NavbarItem/NavbarItem.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavbarItem() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/About">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarItem;
